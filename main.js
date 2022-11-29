fb4add08ff5e2fcade7ced1e4d8eca4317827fae
taylor <taylor.viars@yahoo.com>
2022-11-28 19:30:44 -0500

    commit tree bb147138cdfce9f96ca65d34a6479deca7c777b0

--- main.js
+++ main.js
@@ -3,10 +3,38 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const hearts = document.getElementsByClassName('like-glyph')
+Array.from(hearts).forEach((el) => {
+  el.addEventListener('click', (e) => {
+    mimicServerCall()
+    .then(() => {
+      turnHeartRed(e.target)
+    })
+    .catch((el) => {
+      displayError(el)
+      console.log('fail')
+    })
+  })
+})
 
+function displayError(el){
+  const model = document.querySelector('#modal')
+  model.classList.remove('hidden')
+  const h2 = document.querySelector('h2')
+  h2.textContent = el
+  setTimeout(() => {
+    model.classList.add('hidden')
+  }, 3000)
+}
 
-
-
+function turnHeartRed(e){
+  if(e.textContent === FULL_HEART){
+    e.classList = EMPTY_HEART
+}else{
+  e.textContent = FULL_HEART
+  e.classList.add('activated-heart')
+  }
+}
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
